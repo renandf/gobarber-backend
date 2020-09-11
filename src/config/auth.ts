315833850a3b5b0bc44ab9f4f3a9561156de4065
Renan Castro <renandf@gmail.com>
2020-09-12 07:50:02 +1000

    initial commit with basic functionality

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+	jwt: {
+		secret: "cec25bb0da17d928fc4773b6707217d5",
+		expiresIn: "1d",
+	},
+};
---
+++ src/routes/appointments.routes.ts
@@ -0,0 +1,36 @@
+import { Router, response } from "express";
+import { getCustomRepository } from "typeorm";
+import { parseISO } from "date-fns";
+
+import AppointmentsRepository from "../repositories/AppointmentsRepository";
+import CreateAppointmentService from "../services/CreateAppointmentService";
+
+import ensureAuthenticated from "../middlewares/ensureAuthenticated";
+
+const appoitmentsRouter = Router();
+
+appoitmentsRouter.use(ensureAuthenticated);
+
+appoitmentsRouter.get("/", async (request, response) => {
+	const appointmentsRepository = getCustomRepository(AppointmentsRepository);
+	const appointments = await appointmentsRepository.find();
+
+	return response.json(appointments);
+});
+
+appoitmentsRouter.post("/", async (request, response) => {
+	const { provider_id, date } = request.body;
+
+	const parsedDate = parseISO(date);
+
+	const createAppointment = new CreateAppointmentService();
+
+	const appointment = await createAppointment.execute({
+		provider_id,
+		date: parsedDate,
+	});
+
+	return response.json(appointment);
+});
+
+export default appoitmentsRouter;
