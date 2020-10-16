5944e074820e652a8b40b56ccb64b14b9fa3cfdc
Renan Castro <renandf@gmail.com>
2020-10-17 07:18:39 +1100

    List all providers except requester (logged user)

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 
 import appoitmentsRouter from '@modules/appoitments/infra/http/routes/appointments.routes';
+import providersRouter from '@modules/appoitments/infra/http/routes/providers.routes';
 import usersRouter from '@modules/users/infra/http/routes/users.routes';
 import sessionsRouter from '@modules/users/infra/http/routes/sessions.routes';
 import passwordRouter from '@modules/users/infra/http/routes/password.routes';
@@ -9,6 +10,7 @@ import profileRouter from '@modules/users/infra/http/routes/profile.routes';
 const routes = Router();
 
 routes.use('/appointments', appoitmentsRouter);
+routes.use('/providers', providersRouter);
 routes.use('/users', usersRouter);
 routes.use('/sessions', sessionsRouter);
 routes.use('/password', passwordRouter);
