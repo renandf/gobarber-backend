76ab20b55cbd58ac4f2aa6d2b5e1f5566ed661c9
Renan Castro <renandf@gmail.com>
2020-11-05 10:23:24 +1100

    Cache for ListProviderAppointmentsService to display the hourly appointments in the specific day

--- src/modules/appoitments/services/ListProviderAppointmentsService.ts
+++ src/modules/appoitments/services/ListProviderAppointmentsService.ts
@@ -27,20 +27,24 @@ class ListProviderAppointmentsService {
     month,
     day,
   }: IRequest): Promise<Appointment[]> {
-    const cacheData = await this.cacheProvider.recover('testingKey');
+    const cacheKey = `provider-appointments:${provider_id}:${year}-${month}-${day}`;
 
-    console.log(cacheData);
-
-    const appointments = await this.appointmentsRepository.findHourlyAvailabilityInDay(
-      {
-        provider_id,
-        year,
-        month,
-        day,
-      },
+    let appointments = await this.cacheProvider.recover<Appointment[]>(
+      cacheKey,
     );
 
-    // await this.cacheProvider.save('testingKey', 'testingValue');
+    if (!appointments) {
+      appointments = await this.appointmentsRepository.findHourlyAvailabilityInDay(
+        {
+          provider_id,
+          year,
+          month,
+          day,
+        },
+      );
+
+      await this.cacheProvider.save(cacheKey, appointments);
+    }
 
     return appointments;
   }
